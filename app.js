a23692dcf3d0cdffae1cc3c5f1e428f8c9fa7b1d
max-b10 <bungaymax@gmail.com>
2021-04-07 11:31:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const gridSquares = document.querySelectorAll("#square");
 const startButton = document.querySelector(".start");
 const scoreSpan = document.querySelector("#score");
+const gameContainer = document.querySelector(".game-container");
+const snakeGrid = document.querySelector(".snake-grid");
+// Arrow Keys:
+const upArrow = document.querySelector(".up");
+const downArrow = document.querySelector(".down");
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+
+const gameOverDiv = document.querySelector(".game-over-signal");
 
 // Width represents the longest the snake can be while in a straight line
 // since the grid is 10x10.
 const width = 10;
 let currentIndex = 0; // snake starting position.
 let foodIndex = 0;
-// The snake itself is an array where 2=Headdivs 1=Bodydivs, 0=Taildiv:
+// The snake itself is an array where 2=Headdiv 1=Bodydiv, 0=Taildiv:
 let snake = [2, 1, 0];
 // Direction means the snake will move 1 div along the gridSquares 'array':
 let direction = 1;
@@ -15,9 +24,14 @@ let score = 0;
 let speed = 0.9;
 let intervalTime = 0; // in milliseconds used as a parameter in setInterval method.
 let interval = 0;
+let isGameOver = false;
 
 // Start and restart a game:
 function startGame() {
+  isGameOver = false;
+  gameOverDiv.style.display = "none";
+  snakeGrid.style.background = "#9bba5a";
+  startButton.innerHTML = "Start";
   // Remove previous snake:
   snake.forEach((index) => gridSquares[index].classList.remove("snake"));
   // Remove any food div:
@@ -28,13 +42,14 @@ function startGame() {
 
   // Reset everything:
   direction = 1;
-  scoreSpan.innerHTML = score;
+  scoreSpan.innerHTML = `Score: ${score}`;
   intervalTime = 1000;
   snake = [2, 1, 0];
   currentIndex = 0;
   // Add snake class to the divs in new snake array:
   snake.forEach((index) => gridSquares[index].classList.add("snake"));
   interval = setInterval(movement, intervalTime);
+  startButton.style.display = "none";
 }
 
 function movement() {
@@ -50,6 +65,7 @@ function movement() {
     // snake head hits a snake div:
     gridSquares[snake[0] + direction].classList.contains("snake")
   ) {
+    gameOver();
     return clearInterval(interval);
   }
 
@@ -66,14 +82,23 @@ function movement() {
 
     randomFood();
     score++;
-    scoreSpan.innerHTML = score;
+    scoreSpan.innerHTML = `Score: ${score}`;
     clearInterval(interval);
     intervalTime = intervalTime * speed;
     interval = setInterval(movement, intervalTime);
   }
   gridSquares[snake[0]].classList.add("snake");
 }
-
+function gameOver() {
+  isGameOver = true;
+  console.log("game over");
+  gameOverDiv.style.display = "block";
+  startButton.style.display = "block";
+  scoreSpan.innerHTML = `You scored: ${score}`;
+  startButton.innerHTML = "Restart";
+  gameOverDiv.innerHTML = "Game Over!";
+  snakeGrid.style.background = "#829a50";
+}
 //generate new food each time previous food is 'eaten':
 function randomFood() {
   do {
@@ -92,15 +117,29 @@ function arrowControls(e) {
   } else if (e.code === "ArrowDown") {
     direction = +width;
     console.log("arrow down");
-  } else if (e.code === "ArrowRight") {
-    direction = +1;
-    console.log("arrow right");
   } else if (e.code === "ArrowLeft") {
     direction = -1;
     console.log("arrow left");
+  } else if (e.code === "ArrowRight") {
+    direction = +1;
+    console.log("arrow right");
   }
 }
 document.addEventListener("keyup", arrowControls);
+
+upArrow.addEventListener("click", () => {
+  direction = -width;
+});
+downArrow.addEventListener("click", () => {
+  direction = +width;
+});
+leftArrow.addEventListener("click", () => {
+  direction = -1;
+});
+rightArrow.addEventListener("click", () => {
+  direction = +1;
+});
+
 startButton.addEventListener("click", startGame);
 
 // window.addEventListener("keydown", function (event) {
